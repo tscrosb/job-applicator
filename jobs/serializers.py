eb39e9e4514d413dd8963721dbb5e1a2287ed782
Thomas Crosbie-Walsh <tscrosb@bu.edu>
2025-11-10 17:57:12 +0000

    add job serailizer and databaase operations

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Job
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ["id", "title", "company", "created_at", "updated_at"]
+        extra_kwargs = {"company": {"read_only": True}}
--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serialisers import UserSerializer
+from .serializers import UserSerializer, JobSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Job
 
-# Create your views here.
+class JobListCreate(generics.ListCreateAPIView):
+    serializer_class = JobSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Job.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(company=self.request.company)
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
